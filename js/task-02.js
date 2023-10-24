f49377845dca3eb3fa6352894d77e7023f1969c4
gsrrgm <59813629+gsrrgm@users.noreply.github.com>
2023-10-24 22:06:36 +0300

    add ingredients

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,24 @@
+// Створить окремий елемент <li>.
+// Обов'язково використовуй метод document.createElement().
+// Додасть назву інгредієнта як його текстовий вміст.
+// Додасть елементу клас item.
+// Після чого, вставить усі <li> за одну операцію у 
+// список ul#ingredients.
+
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+ingredients.forEach(element => {
+  const liEl = document.createElement('li')
+  const ulEl = document.querySelector("#ingredients")
+  liEl.textContent = element 
+  liEl.classList.add('item')
+  ulEl.append(liEl)
+  console.log(liEl)
+});
