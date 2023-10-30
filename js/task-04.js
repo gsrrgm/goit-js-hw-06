784d081250881eac24daf04c2d62639afcb4789c
gsrrgm <59813629+gsrrgm@users.noreply.github.com>
2023-10-30 17:46:18 +0200

    update task-4

---
+++ js/task-04.js
@@ -0,0 +1,18 @@
+// Створи змінну counterValue, в якій буде зберігатися поточне значення лічильника та ініціалізуй її значенням 0.
+// Додай слухачів кліків до кнопок, всередині яких збільшуй або зменшуй значення лічильника.
+// Оновлюй інтерфейс новим значенням змінної counterValue.
+
+let counterValue = 0;
+const counter = document.querySelector("#counter");
+const handleClick = (event) => {
+  if (!event.target.hasAttribute("data-action")) {
+    return;
+  }
+
+  counterValue += Number(event.target.textContent);
+  const value = document.querySelector("#value");
+  value.textContent = counterValue;
+
+};
+
+counter.addEventListener("click", handleClick);
