a5199d834888f6da6390a73585cf7395e1972247
gsrrgm <59813629+gsrrgm@users.noreply.github.com>
2023-11-01 16:39:19 +0200

    update

--- js/task-06.js
+++ js/task-06.js
@@ -1,4 +1,4 @@
-// 1). під час втрати фокусу на інпуті (подія blur), 
+// 1). під час втрати фокусу на інпуті (подія blur),
 // перевіряє його вміст щодо правильної кількості введених символів.
 
 // <input
@@ -10,9 +10,9 @@
 
 // кількість символів в інпуті, в його атрибуті data-length.
 
-// правильна кількість символів, то border інпуту стає зеленим, 
+// правильна кількість символів, то border інпуту стає зеленим,
 // якщо неправильна кількість - червоним.
-// Для додавання стилів використовуй CSS-класи valid і invalid, 
+// Для додавання стилів використовуй CSS-класи valid і invalid,
 // які ми вже додали у вихідні файли завдання.
 
 // #validation-input {
@@ -23,16 +23,18 @@
 //   border-color: #4caf50;
 // }
 
-// #validation-input.invalid {
+//  #validation-input.invalid {
 //   border-color: #f44336;
-// }
-  
+//  }
 
-const inpEl = document.querySelector('#validation-input')
-inpEl.style.border = '3px solid #bdbdbd'
-inpEl.addEventListener('blur', () => {
-    if(inpEl.value.length === Number(inpEl.dataset.length)){
-        inpEl.style.borderColor = "#4caf50"
-         } else {inpEl.style.borderColor = "#f44336"}
-         
-})
+const inpEl = document.querySelector("#validation-input");
+
+inpEl.addEventListener("blur", (event) => {
+  const inputLength = event.currentTarget.value.length;
+  const dataLength = Number(event.currentTarget.dataset.length);
+  if (inputLength === dataLength) {
+    inpEl.classList.replace("invalid", "valid");
+    return;
+  }
+  inpEl.classList.add("invalid");
+});
